bc1ca4e0e0d60dad0973571aa6d75842d48f6b27
PasteAsawari <149721356+PasteAsawari@users.noreply.github.com>
2024-09-15 20:29:24 +0530

    Tutorial -3
     How to see website in Mobile

---
+++ Tutorial-3/script.js
@@ -0,0 +1,7 @@
+alert("WELCOME!")
+
+// steps to see website on a phone
+// 1. get IPv4 address using ipconfig command on terminal
+// 2. put it in live preview
+// 3 .make the internet network private
+// 4. enter the ip and port in phone and wollah you are good to go
